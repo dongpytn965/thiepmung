5ed8d15466b730d7f71c0c2a5d1d7631daa21c05
dongpytn965 <116258473+dongpytn965@users.noreply.github.com>
2022-10-20 14:39:39 +0700

    Add files via upload
    creat

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'N.H.Hậu';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng Hậu 20/10';
+const contentCard = 'Chúc Cô Hậu 20/10 vui vẻ, Chúc em luôn luôn xinh đẹp, hạnh phúc và thành công trong cuộc sống';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
